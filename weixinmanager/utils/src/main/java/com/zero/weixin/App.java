8b79708316f998916ae3e315aab6a67fa7ff6eed
baojian <13466659100@139.com>
2016-02-02 14:47:01 +0800

    ok

---
+++ weixinmanager/utils/src/main/java/com/zero/weixin/App.java
@@ -0,0 +1,13 @@
+package com.zero.weixin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
